9265930265303952429901e17209b1cec71c9b91
Afo <57149309+afoxie@users.noreply.github.com>
2021-08-24 03:20:16 -0400

    Yiff update!
    The pictures are in the .gitignore so you would have to query the API to see them...

--- server.go
+++ server.go
@@ -13,6 +13,14 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var port string = "8080" // have to port-forward this
+
+func random_fn(category string) (string, int) {
+	files, _ := ioutil.ReadDir("./imgs/" + category)
+	_selection := rand.Intn(len(files)-1) + 1
+	return files[_selection].Name(), _selection
+}
+
 type img_response struct {
 	Id  string `json:"id"`
 	Url string `json:"url"`
@@ -24,19 +32,31 @@ func default_enpoint(w http.ResponseWriter, r *http.Request) {
 
 func fox_endpoint(w http.ResponseWriter, r *http.Request) {
 	rand.Seed(time.Now().UnixNano())
-	_id := strconv.Itoa(rand.Intn(num_fox-1) + 1)
-	_url := "http://" + r.Host + "/img/fox/fox_" + _id + ".jpg"
+	_fn, _id := random_fn("fox")
+	_url := "https://" + r.Host + "/img/fox/" + _fn
 	json.NewEncoder(w).Encode([]img_response{
-		{Id: _id, Url: _url},
+		{Id: strconv.Itoa(_id), Url: _url},
+	})
+}
+func yiff_endpoint(w http.ResponseWriter, r *http.Request) {
+	rand.Seed(time.Now().UnixNano())
+	_fn, _id := random_fn("fox")
+	_url := "https://" + r.Host + "/img/yiff/" + _fn
+	json.NewEncoder(w).Encode([]img_response{
+		{Id: strconv.Itoa(_id), Url: _url},
 	})
 }
 
 var num_fox int
+var num_yiff int
 
 func main() {
 	files, _ := ioutil.ReadDir("./imgs/fox")
 	num_fox = len(files)
 	fmt.Println("Currently serving", num_fox, "pictures of foxes :3")
+	files, _ = ioutil.ReadDir("./imgs/yiff")
+	num_yiff = len(files)
+	fmt.Println("Currently serving", num_yiff, "yiff and counting owo")
 
 	/* router */
 	router := mux.NewRouter().StrictSlash(true)
@@ -45,6 +65,7 @@ func main() {
 	router.Handle("/", http.FileServer(http.Dir("./static/")))
 	/* dynamic */
 	router.HandleFunc("/fox", fox_endpoint)
+	router.HandleFunc("/yiff", yiff_endpoint)
 	/* 404 */
 	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "afo suck cock")
@@ -54,5 +75,7 @@ func main() {
 		fmt.Fprintf(w, "Method not allowed")
 	})
 
-	log.Fatal(http.ListenAndServe(":8080", router))
+	/* listen */
+	log.Fatal(http.ListenAndServe(":"+port, router))
+	fmt.Println("Listening on port", port)
 }

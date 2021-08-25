5f13170a928a321544365507236d5a6bca52f260
Afo <57149309+afoxie@users.noreply.github.com>
2021-08-25 16:00:42 -0400

    ihateeverything

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"math/rand"
@@ -15,10 +16,68 @@ import (
 
 var port string = "8080" // have to port-forward this
 
+var wc http.Client = http.Client{}
+
 func random_fn(category string) (string, int) {
-	files, _ := ioutil.ReadDir("./imgs/" + category)
-	_selection := rand.Intn(len(files)-1) + 1
-	return files[_selection].Name(), _selection
+	_files, _ := ioutil.ReadDir("./imgs/" + category)
+	_selection := rand.Intn(len(_files)-1) + 1
+	return _files[_selection].Name(), _selection
+}
+
+type reddit_post struct {
+	title           string
+	thumbnail       string
+	link_flair_type string
+	nsfw            bool
+	pinned          bool
+	is_mod_post     bool
+	author          string
+	image_url       string
+}
+type reddit_api_response struct {
+	Data struct {
+		Children []struct {
+			Data struct {
+				Title         string                `json:"title"`
+				Thumbnail     string                `json:"thumbnail"`
+				LinkFlairType string                `json:"link_flair_type"`
+				Distinguished string                `json:"distinguished"`
+				Author        string                `json:"author"`
+				ImageUrl      string                `json:"url_url_overriden_by_dest"`
+				Preview       map[int64]interface{} `json:"preview"`
+			} `json:"data"`
+		} `json:"children"`
+	} `json:"data"`
+}
+
+func reddit(subreddit string) []reddit_post {
+	_url := "https://www.reddit.com/r/" + subreddit + ".json"
+	_req, _err := http.NewRequest("GET", _url, nil)
+	_req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36 OPR/78.0.4093.153")
+	_req.Header.Add("accept", "application/json;q=0.9")
+	_req.Header.Add("accept-encoding", "*")
+	_resp, _err := wc.Do(_req)
+	if _err != nil {
+		// handle it or something
+	}
+	defer _resp.Body.Close()
+	_body, _err := io.ReadAll(_resp.Body)
+	var _json reddit_api_response
+	json.Unmarshal(_body, &_json)
+	_posts := _json.Data.Children
+	_returns := []reddit_post{}
+	for _, _child := range _posts {
+		_post := _child.Data
+		fmt.Println(_post.Preview[0].(map[string]string))
+		_returns = append(_returns, reddit_post{
+			title:       _post.Title,
+			author:      _post.Author,
+			thumbnail:   _post.Thumbnail,
+			image_url:   _post.ImageUrl,
+			is_mod_post: (_post.Distinguished == "moderator"),
+		})
+	}
+	return _returns
 }
 
 type img_response struct {
@@ -28,6 +87,15 @@ type img_response struct {
 
 func default_enpoint(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "afo suck cock")
+}
+
+func reddit_endpoint(w http.ResponseWriter, r *http.Request) {
+	_mode := r.URL.Query().Get("type")
+	_subreddit := "yiff"
+	if _mode == "gay" {
+		_subreddit = "gfur"
+	}
+	json.NewEncoder(w).Encode(reddit(_subreddit))
 }
 
 func fox_endpoint(w http.ResponseWriter, r *http.Request) {
@@ -47,6 +115,17 @@ func yiff_endpoint(w http.ResponseWriter, r *http.Request) {
 	})
 }
 
+func middleware(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Method, r.URL, "FROM", r.Header.Get("x-forwarded-for"), " AS \""+r.Header.Get("user-agent")+"\"")
+		auth := r.URL.Query().Get("api_key")
+		if auth != "" {
+			fmt.Println("WITH AUTH KEY ", auth)
+		}
+		h.ServeHTTP(w, r)
+	})
+}
+
 var num_fox int
 var num_yiff int
 
@@ -63,6 +142,7 @@ func main() {
 	/* dynamic */
 	router.HandleFunc("/fox", fox_endpoint)
 	router.HandleFunc("/yiff", yiff_endpoint)
+	router.HandleFunc("/reddit", reddit_endpoint)
 	/* static */
 	router.PathPrefix("/img/").Handler(http.StripPrefix("/img/", http.FileServer(http.Dir("./imgs/"))))
 	router.PathPrefix("/demo/").Handler(http.StripPrefix("/demo/", http.FileServer(http.Dir("./static/demo/"))))
@@ -75,8 +155,7 @@ func main() {
 	router.MethodNotAllowedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Method not allowed")
 	})
-
 	/* listen */
-	log.Fatal(http.ListenAndServe(":"+port, router))
 	fmt.Println("Listening on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, middleware(router)))
 }
